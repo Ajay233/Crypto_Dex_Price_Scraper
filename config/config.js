dd0154f0f8f08f22f2e8d61cea6f6fd429a97c6a
Ajay Mungur <ajaymungur@hotmail.com>
2022-02-27 21:44:57 +0000

    Set up environment variables and environment switching
    - Installed dotenv, win-node-env and mysql connector
    - Created an (untracked) env file for environment variables
    - Created a config file which references the relevent environment
    variables based on which environment is currently set
    - Updated the package.json file so that the correct environment will be
    set for development and production.

---
+++ config/config.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+module.exports = {
+  "development": {
+    "username": process.env.DEV_DB_USERNAME,
+    "password": process.env.DEV_DB_PASSWORD,
+    "database": process.env.DEV_DB_NAME,
+    "host": process.env.DEV_DB_HOST,
+    "port": process.env.DEV_DB_PORT,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": process.env.TEST_DB_USERNAME,
+    "password": process.env.TEST_DB_PASSWORD,
+    "database": process.env.TEST_DB_NAME,
+    "host": process.env.TEST_DB_HOST,
+    "port": process.env.TEST_DB_PORT,
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": process.env.PROD_DB_USERNAME,
+    "password": process.env.PROD_DB_PASSWORD,
+    "database": process.env.PROD_DB_NAME,
+    "host": process.env.PROD_DB_HOST,
+    "port": process.env.PROD_DB_PORT,
+    "dialect": "mysql"
+  }
+}
